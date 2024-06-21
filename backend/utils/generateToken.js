dfe281824aac9fe7c76f7c27f6a2c7ed328f242f
hashmishaikhmxl <hashmi.shaikh@mexcelle.com>
2024-06-21 18:27:10 +0530

    generate token change

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -8,6 +8,7 @@ const generateTokenAndSetToken = (userId, res) => {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true, //prevent XSS attack cross-site scripting attack
     sameSite: "strict",
+    secure: process.env.NODE_ENV !== "development"
   });
 };
 
